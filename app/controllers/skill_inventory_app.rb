f3f73bf192e0e38ba85904972aa731c433ecff2b
Nicole Marino <nicolekmarino@hotmail.com>
2016-06-29 22:22:52 -0600

    first commit

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,8 +1,45 @@
+require "models/skill_inventory"
+
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
   get '/skills' do
+    @skills  = skill_inventory.all
     erb :index
   end
+
+  get '/' do
+    erb :main
+  end
+
+
+  get '/skills/new' do
+    erb :new
+  end
+
+  post '/skills' do
+    skill_inventory.create(params[:skill])
+    redirect "/skills"
+  end
+
+  get '/skills/:id' do |id|
+    @skills = skill_inventory.find(id.to_i)
+    erb :show
+  end
+
+  get '/skills/:id/edit' do |id|
+  @task = skill_inventory.find(id.to_i)
+  erb :edit
+  end
+
+  get '/skills/clear' do
+  skill_inventory.delete_all
+  redirect "/"
+  end
+
+  def skill_inventory
+    @skill_inventory ||= SkillInventory.new
+  end
+
 
 end
--- app/models/skill_inventory.rb
+++ app/models/skill_inventory.rb
@@ -1,5 +1,49 @@
+require 'yaml/store'
+require_relative 'skill'
+
 class SkillInventory
-  def self.database
-    @database ||= YAML::Store.new("db/skill_inventory")
+  attr_reader :database
+  def initialize
+    @database= database_creator
   end
+
+
+    def database_creator
+      database ||= YAML::Store.new("db/skill_inventory")
+    end
+
+    def create(skill)
+      @database.transaction do
+        database['skills'] ||= []
+        database['total'] ||= 0
+        database['total'] += 1
+        database['skills'] << { "id" => database['total'], "title" => skill[:title], "description" => skill[:description] }
+      end
+    end
+
+  def raw_skills
+    @database.transaction do
+      database['skills'] || []
+    end
+  end
+
+  def all
+    raw_skills.map { |data| Skill.new(data) }
+  end
+
+  def raw_skill(id)
+    raw_skills.find { |skill| skill["id"] == id }
+  end
+
+  def find(id)
+    Skill.new(raw_skill(id))
+  end
+
+  def delete_all
+    @database.transaction do
+      database["skills"] = []
+      database["total"] = 0
+    end
+  end
+
 end
